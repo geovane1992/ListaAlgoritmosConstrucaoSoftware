5b7c497ec6311dabc0ed052038b7c035c45eb337
GAOliveira <GAOliveira@INF04GO.gsa.go>
2015-11-05 20:50:22 -0200

    Retirando comentários desnecessários.

--- Algoritmo3/src/test/java/br/ufg/algoritmo3/somaNaturaisTest.java
+++ Algoritmo3/src/test/java/br/ufg/algoritmo3/somaNaturaisTest.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.ufg.algoritmo3;
 
 import br.ufg.algoritmo3.somaNaturais;
@@ -13,10 +9,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author GAOliveira
- */
+
 public class somaNaturaisTest {
     
     @org.junit.Test
