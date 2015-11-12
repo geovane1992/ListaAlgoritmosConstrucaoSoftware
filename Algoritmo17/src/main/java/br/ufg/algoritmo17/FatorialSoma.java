566dda00f4f06362bd635a73ecac74ec6e139eed
GAOliveira <GAOliveira@INF04GO.gsa.go>
2015-11-12 18:28:25 -0200

    Correção arquivo algoritmo17

--- Algoritmo17/src/main/java/br/ufg/algoritmo17/FatorialSoma.java
+++ Algoritmo17/src/main/java/br/ufg/algoritmo17/FatorialSoma.java
@@ -1,17 +1,17 @@
 package br.ufg.algoritmo17;
 
 /**
- * M√©todo criado para calcular fatorial.
+ * MÈtodo criado para calcular fatorial.
  */
 
 public class FatorialSoma {
     
      /**
-     * @param n v√°ri√°vel que define o valor de entrada passado por parametro. 
+     * @param n v·ri·vel que define o valor de entrada passado por parametro. 
      *
      * @return O fatorial do valor de entrada
      * 
-     * @throws IllegalArgumentException Caso a vari√°vel n receba um valor menor que 1 entrar√° no Exception .
+     * @throws IllegalArgumentException Caso a vari·vel n receba um valor menor que 1 entrar· no Exception .
      */
     
     public static double fatorialSoma(int n){
@@ -21,7 +21,7 @@ public class FatorialSoma {
         int contador = 2;
         int fatorial = 1;
         
-        while(contador <= n){
+        while(contador <= n) {
             fatorial = br.ufg.algoritmo4.Produto.produto(fatorial,contador);
             contador = contador + 1;
         }
