6a7d726faba21ac80a777cc095da57938834e86f
GAOliveira <GAOliveira@INF04GO.gsa.go>
2015-11-12 21:10:15 -0200

    Correção arquivo algoritmo43

--- Algoritmo43/src/test/java/br/ufg/algoritmo43/PermutacaoTest.java
+++ Algoritmo43/src/test/java/br/ufg/algoritmo43/PermutacaoTest.java
@@ -13,14 +13,14 @@ import static org.junit.Assert.*;
 public class PermutacaoTest {
     
     @Test
-    public static void testCasoTrivial(){
+    public void testCasoTrivial(){
       List<String> retorno = Permutacao.permutacao("", "a", new ArrayList<String>() );
         Assert.assertEquals(1, retorno.size());
         Assert.assertEquals("a", retorno.get(0));
     }
     
     @Test
-    public static void testOutroCasoTrivial(){
+    public  void testOutroCasoTrivial(){
       List<String> retorno = Permutacao.permutacao("", "ab", new ArrayList<String>() );
         Assert.assertEquals(2, retorno.size());
         Assert.assertTrue(retorno.contains("ab"));
@@ -28,11 +28,11 @@ public class PermutacaoTest {
     }
     
     @Test
-    public static void testCasoTrivial3(){
+    public  void testCasoTrivial3(){
       List<String> retorno = Permutacao.permutacao("", "abcd", new ArrayList<String>() );
         Assert.assertEquals(24, retorno.size());
-        Assert.assertTrue(retorno.contains("ab"));
-        Assert.assertTrue(retorno.contains("ba"));
+        Assert.assertTrue(retorno.contains("abcd"));
+        Assert.assertTrue(retorno.contains("bacd"));
     }
     
 }
